f02a93d5e73e06af73bb100e56f2ef487446b6f1
William Wong <compulim@hotmail.com>
2017-03-02 10:50:15 +0800

    Redirection logic

--- github/index.js
+++ github/index.js
@@ -1,13 +1,13 @@
 const githubUrlPattern = /^https:\/\/github.com\/([^\/]+)\/([^\/]+)(\/tree\/([^\/]+))?/;
 
 module.exports = function (context, req) {
-  const referer = req.headers.Referer;
+  const referer = req.headers.referer;
 
   if (!referer) {
     context.log('No "Referer" in header');
 
     return context.done(null, {
-      status: 404,
+      status: 400,
       body: 'Please add this link to README.md in your GitHub'
     });
   }
@@ -20,13 +20,25 @@ module.exports = function (context, req) {
     context.log('Not a GitHub referer');
 
     return context.done(null, {
-      status: 404,
+      status: 400,
       body: 'You can only put this link in a GitHub repository'
     });
   }
 
+  const [account, repository, _, ref] = match;
+
+  context.log(`GitHub account: ${ account }`);
+  context.log(`Repository: ${ repository }`);
+  context.log(`Ref: ${ ref }`);
+
+  const urlToJSON = `https://raw.githubusercontent.com/${ account }/${ repository }/tree/${ ref || 'master' }/azuredeploy.json`);
+
+  context.log(`Template URL: ${ urlToJSON }`);
+
   return context.done(null, {
-    status: 200,
-    body: 'Hello, World!'
+    status: 302,
+    headers: {
+      location: `https://portal.azure.com/#create/Microsoft.Template/uri/${ encodeURI(urlToJSON) }`
+    }
   });
 };
